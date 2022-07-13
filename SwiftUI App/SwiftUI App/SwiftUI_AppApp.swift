2190e75c978968a243e4101400dedcd545c4daa7
Alberto Fernández-Baíllo <alberto.fernandezb@i3television.es>
2022-07-13 18:02:16 +0200

    Shape Stroke and blend mode

---
+++ SwiftUI App/SwiftUI App/SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppApp.swift
+//  SwiftUI App
+//
+//  Created by Alberto Fernández-Baillo Rodríguez on 13/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
